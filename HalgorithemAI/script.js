5c5c9d527dbe08a7cb1e83ea1fe9d7655f679e0e
Reyaansh Sinha <reyaanshsinha@Reyaanshs-MacBook-Pro-2.local>
2026-05-06 15:46:01 -0400

    Added better readme ad stuff

--- HalgorithemAI/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".navbar-nav .nav-link").forEach(link => {
-  link.addEventListener("click", () => {
-    const menu = document.querySelector(".navbar-collapse.show");
-    if (!menu) return;
-
-    const collapse = bootstrap.Collapse.getOrCreateInstance(menu);
-    collapse.hide();
-  });
-});
